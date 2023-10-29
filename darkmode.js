25ec0baf2726fc131796666144de39169d1372af
abifonziuade <142629317+abifonziuade@users.noreply.github.com>
2023-10-29 14:56:59 -0300

    cambios darkmode mobile

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,8 @@
 const body = document.querySelector('body');
 const btn = document.querySelector('.btn');
+const btn_mobile = document.querySelector('.btn-mobile');
 const icon = document.querySelector('.btn__icon');
+const icon_mobile = document.querySelector('.btn__icon-mobile');
 
 //funcion que se ejecuta al cargar la pagina
 function load(){
@@ -11,11 +13,14 @@ function load(){
   if(!darkmode){
     localStorage.setItem('darkmode', false);
     icon.classList.add('fa-sun');
+    icon_mobile.classList.add('fa-sun');
   } else if( darkmode == 'true'){ //chequea si el darkmode esta activado
     body.classList.add('darkmode');
     icon.classList.add('fa-moon');
+    icon_mobile.classList.add('fa-moon');
   } else if(darkmode == 'false'){ //chequea si el darkmode esta desactivado
     icon.classList.add('fa-sun');
+    icon_mobile.classList.add('fa-sun');
   }
 }
 
@@ -34,5 +39,21 @@ btn.addEventListener('click', () => {
   }else{
     icon.classList.remove('fa-moon');
     icon.classList.add('fa-sun');
+  }
+})
+
+btn_mobile.addEventListener('click', () => {
+    // el toggle agrega o saca la clase darkmode del body
+  body.classList.toggle('darkmode');
+
+  //guarda en localstorage si se activó o desactivó el darkmode
+  localStorage.setItem('darkmode', body.classList.contains('darkmode'));
+
+  if(body.classList.contains('darkmode')){
+    icon_mobile.classList.remove('fa-sun');
+    icon_mobile.classList.add('fa-moon');
+  }else{
+    icon_mobile.classList.remove('fa-moon');
+    icon_mobile.classList.add('fa-sun');
   }
 })
